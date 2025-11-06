1af6897ef0169c9da9404e8433f384c1390e5df0
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-06 11:36:23 +0100

    Create TodoController
    - GET /api/todo
    - Post /api/todo

--- src/main/java/org/stefanneuberger/recap4/controller/TodoController.java
+++ src/main/java/org/stefanneuberger/recap4/controller/TodoController.java
@@ -1,4 +1,32 @@
 package org.stefanneuberger.recap4.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.stefanneuberger.recap4.model.Todo;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
 public class TodoController {
+
+    @GetMapping("")
+    @Operation(
+            summary = "Get todos",
+            description = "Returns a list of all todos"
+    )
+    public ResponseEntity<List<Todo>> getTodos() {
+        return null;
+    }
+
+    @PostMapping("")
+    @Operation(
+            summary = "Create todo",
+            description = "Creates a new todo"
+    )
+    public ResponseEntity<Todo> createTodo(@RequestBody final Todo todo) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+    }
 }
