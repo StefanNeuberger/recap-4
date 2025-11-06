adfa7232df46d5a07f2d6d8e7e895b3a3bdc26ef
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-06 14:31:25 +0100

    add tests
    - getTodoById
    - deleteTodo

--- src/test/java/org/stefanneuberger/recap4/service/TodoServiceTest.java
+++ src/test/java/org/stefanneuberger/recap4/service/TodoServiceTest.java
@@ -6,16 +6,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.stefanneuberger.recap4.dto.CreateTodoDto;
+import org.stefanneuberger.recap4.exception.ResourceNotFoundException;
 import org.stefanneuberger.recap4.model.Todo;
 import org.stefanneuberger.recap4.repository.TodoRepository;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
@@ -56,6 +59,72 @@ class TodoServiceTest {
         assertEquals(expectedTodos.size(), actualTodos.size());
         assertEquals(expectedTodos, actualTodos);
         verify(todoRepository).findAll();
+    }
+
+
+    @Test
+    void getTodoById_shouldReturnTodo_whenTodoExists() {
+        // GIVEN
+        String todoId = "123";
+        Todo expectedTodo = new Todo("test", Todo.Status.OPEN);
+        when(todoRepository.findById(todoId)).thenReturn(Optional.of(expectedTodo));
+
+        // WHEN
+        Todo actualTodo = todoService.getTodoById(todoId);
+
+        // THEN
+        assertEquals(expectedTodo, actualTodo);
+        verify(todoRepository).findById(todoId);
+    }
+
+    @Test
+    void getTodoById_shouldThrowResourceNotFoundException_whenTodoDoesNotExist() {
+        // GIVEN
+        String todoId = "999";
+        when(todoRepository.findById(todoId)).thenReturn(Optional.empty());
+
+        // WHEN
+
+        // THEN
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
+            todoService.getTodoById(todoId);
+        });
+
+        assertEquals("Todo with id " + todoId + " not found", exception.getMessage());
+        verify(todoRepository).findById(todoId);
+    }
+
+    @Test
+    void deleteTodo_shouldDeleteTodo_whenTodoExists() {
+        // GIVEN
+        String todoId = "123";
+        Todo todo = new Todo("test", Todo.Status.OPEN);
+        todo.setId(todoId);
+        when(todoRepository.findById(todoId)).thenReturn(Optional.of(todo));
+        doNothing().when(todoRepository).deleteById(todoId);
+
+        // WHEN
+        todoService.deleteTodo(todoId);
+
+        // THEN
+        verify(todoRepository).findById(todoId);
+        verify(todoRepository).deleteById(todoId);
+    }
+
+    @Test
+    void deleteTodo_shouldThrowResourceNotFoundException_whenTodoDoesNotExist() {
+        // GIVEN
+        String todoId = "999";
+        when(todoRepository.findById(todoId)).thenReturn(Optional.empty());
+
+        // WHEN & THEN
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
+            todoService.deleteTodo(todoId);
+        });
+
+        assertEquals("Todo with id " + todoId + " not found", exception.getMessage());
+        verify(todoRepository).findById(todoId);
+        verify(todoRepository, never()).deleteById(anyString());
     }
 
 
