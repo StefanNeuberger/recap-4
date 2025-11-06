98ee09628e601ebd05a1e7969ead3e870a1687bc
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-06 13:12:36 +0100

    Update TodoController
    - GET /{id}
    - DELETE /{id}
    - PATCH /{id}

--- src/main/java/org/stefanneuberger/recap4/controller/TodoController.java
+++ src/main/java/org/stefanneuberger/recap4/controller/TodoController.java
@@ -1,10 +1,18 @@
 package org.stefanneuberger.recap4.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.stefanneuberger.recap4.dto.CreateTodoDto;
 import org.stefanneuberger.recap4.model.Todo;
+import org.stefanneuberger.recap4.service.TodoService;
 
 import java.util.List;
 
@@ -12,13 +20,28 @@ import java.util.List;
 @RequestMapping("/api/todo")
 public class TodoController {
 
+    private final TodoService todoService;
+
+    public TodoController(final TodoService todoService) {
+        this.todoService = todoService;
+    }
+
     @GetMapping("")
     @Operation(
             summary = "Get todos",
             description = "Returns a list of all todos"
     )
     public ResponseEntity<List<Todo>> getTodos() {
-        return null;
+        return ResponseEntity.ok(todoService.getTodos());
+    }
+
+    @GetMapping("/{id}")
+    @Operation(
+            summary = "Get todo",
+            description = "Returns specific todo"
+    )
+    public ResponseEntity<Todo> getTodo(@PathVariable String id) {
+        return ResponseEntity.ok(todoService.getTodoById(id));
     }
 
     @PostMapping("")
@@ -26,7 +49,46 @@ public class TodoController {
             summary = "Create todo",
             description = "Creates a new todo"
     )
-    public ResponseEntity<Todo> createTodo(@RequestBody final Todo todo) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+    public ResponseEntity<Todo> createTodo(
+            @RequestBody(
+                    description = "Todo creation data", required = true,
+                    content = @Content(schema = @Schema(implementation = CreateTodoDto.class)))
+            @org.springframework.web.bind.annotation.RequestBody CreateTodoDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.createTodo(dto));
     }
+
+    @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Delete todo",
+            description = "Deletes todo by id"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Todo deleted"),
+            @ApiResponse(responseCode = "404", description = "Todo not found",
+                    content = @Content(schema = @Schema(implementation = Error.class))),
+    })
+    public ResponseEntity<Void> deleteTodo(
+            @Parameter(description = "Image ID", required = true, example = "507f1f77bcf86cd799439011")
+            @PathVariable String id) {
+        todoService.deleteTodo(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    @Operation(
+            summary = "Update todo",
+            description = "Update todo by id"
+    )
+    public ResponseEntity<Todo> updateTodo(
+            @Parameter(description = "Todo ID", required = true, example = "507f1f77bcf86cd799439011")
+            @PathVariable String id,
+            @RequestBody(
+                    description = "Todo creation data", required = true,
+                    content = @Content(schema = @Schema(implementation = CreateTodoDto.class)))
+            @org.springframework.web.bind.annotation.RequestBody CreateTodoDto dto) {
+        Todo todo = todoService.updateTodo(id, dto);
+        return ResponseEntity.ok(todo);
+    }
+
+
 }
