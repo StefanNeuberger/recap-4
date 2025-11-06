8daf7fcdc1f8c73a2f6a090e59426ac7265e88d7
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-06 10:57:31 +0100

    initial commit with frontend

---
+++ src/main/java/org/stefanneuberger/recap4/Recap4Application.java
@@ -0,0 +1,13 @@
+package org.stefanneuberger.recap4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recap4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recap4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/stefanneuberger/recap4/Recap4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stefanneuberger.recap4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recap4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
