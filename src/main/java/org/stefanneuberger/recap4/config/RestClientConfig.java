575979782ceea388f1352ee244b3cd57e9f65878
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-11 11:50:24 +0100

    cleaning up

--- src/main/java/org/stefanneuberger/recap4/config/RestClientConfig.java
+++ src/main/java/org/stefanneuberger/recap4/config/RestClientConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
-    @Bean
     public RestClient restClient(final RestClient.Builder builder) {
         return builder.build();
     }
